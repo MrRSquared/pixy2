fb4c0f427d928ed057bf14736dd416caa69d7894
Mr. Roth-Ritchie <jrothritchie@hdsd.k12.nh.us>
2018-12-21 17:54:18 -0500

    Fixed the pan/tilt for Python 3
    Moved the round() functions so the pan/tilt PID is now working.

--- src/host/libpixyusb2_examples/python_demos/pan_tilt_demo.py
+++ src/host/libpixyusb2_examples/python_demos/pan_tilt_demo.py
@@ -5,15 +5,15 @@ from pixy import *
 # Pixy2 Python SWIG pan/tilt demo #
 
 # Constants #
-PID_MAXIMUM_INTEGRAL      = 2000
+PID_MAXIMUM_INTEGRAL      =  2000
 PID_MINIMUM_INTEGRAL      = -2000
-ZUMO_BASE_DEADBAND        =   20
-PIXY_RCS_MAXIMUM_POSITION = 1000
+ZUMO_BASE_DEADBAND        =    20
+PIXY_RCS_MAXIMUM_POSITION =  1000
 PIXY_RCS_MINIMUM_POSITION =     0
 PIXY_RCS_CENTER_POSITION  = ((PIXY_RCS_MAXIMUM_POSITION - PIXY_RCS_MINIMUM_POSITION) / 2)
 MINIMUM_BLOCK_AGE_TO_LOCK =    30
-PAN_GAIN                  =  400
-TILT_GAIN                 =  500
+PAN_GAIN                  =   400
+TILT_GAIN                 =   500
 
 def Reset ():
   global Locked_On_Block
@@ -21,8 +21,8 @@ def Reset ():
   Locked_On_Block    = False
   Locked_Block_Index = 0
 
-def Display_Block (Index, Block):
-        print(('                   Block[%3d]: I: %3d / S:%2d / X:%3d / Y:%3d / W:%3d / H:%3d / A:%3d' % (Index, Block.m_index, Block.m_signature, Block.m_x, Block.m_y, Block.m_width, Block.m_height, Block.m_age)))
+def Display_Block (Index, Block,P):
+        print ('                   Block[%3d]: I: %3d / S:%2d / X:%3d / Y:%3d / W:%3d / H:%3d / A:%3d /P:%3d' % (Index, Block.m_index, Block.m_signature, Block.m_x, Block.m_y, Block.m_width, Block.m_height, Block.m_age,P))
 
 class PID_Controller:
   def __init__ (self, Proportion_Gain, Integral_Gain, Derivative_Gain, Servo):
@@ -33,7 +33,7 @@ class PID_Controller:
     self.Reset ()
 
   def Reset (self):
-    self.Previous_Error  =  0x80000000
+    self.Previous_Error  = 0x7fffffff
     self.Integral_Value  = 0
     if self.Servo:
       self.Command = PIXY_RCS_CENTER_POSITION
@@ -43,7 +43,7 @@ class PID_Controller:
   def Update (self, Error):
     PID = 0
 
-    if self.Previous_Error !=  0x80000000:
+    if self.Previous_Error !=  0x7fffffff:
       # Update integral component #
       self.Integral_Value = self.Integral_Value + Error
 
@@ -54,9 +54,8 @@ class PID_Controller:
         self.Integral_Value = PID_MINIMUM_INTEGRAL
 
       # Calculate Proportion, Integral, Derivative (PID) term #
-      PID = (Error * self.Proportion_Gain + ((self.Integral_Value * self.Integral_Gain) >> 4) + (Error - self.Previous_Error) * self.Derivative_Gain) >> 10;
-      #(Error * self.Proportion_Gain + ((self.Integral_Value * self.Integral_Gain)) + (Error - self.Previous_Error) * self.Derivative_Gain);
-
+      PID = round (Error * self.Proportion_Gain + (round(self.Integral_Value * self.Integral_Gain) >> 4) + (Error - self.Previous_Error) * self.Derivative_Gain) >> 10;
+      print (PID)
       if self.Servo:
         # Integrate the PID term because the servo is a position device #
         self.Command = self.Command + PID
@@ -103,8 +102,8 @@ while 1:
       # Find the block that we are locked to #
       for Index in range (0, Count):
         if Blocks[Index].m_index == Locked_Block_Index:
-          print('Frame %3d: Locked' % (Frame))
-          Display_Block (Index, Blocks[Index])
+          print ('Frame %3d: Locked' % (Frame))
+          Display_Block (Index, Blocks[Index], Pan_PID_Controller.Command)
 
           Pan_Offset  = (pixy.get_frame_width () / 2) - Blocks[Index].m_x;
           Tilt_Offset = Blocks[Index].m_y - (pixy.get_frame_height () / 2)
@@ -112,13 +111,14 @@ while 1:
           Pan_PID_Controller.Update (Pan_Offset)
           Tilt_PID_Controller.Update (Tilt_Offset)
 
-          pixy.set_servos (int(round(Pan_PID_Controller.Command)), int(round(Tilt_PID_Controller.Command)))
+          pixy.set_servos (round(Pan_PID_Controller.Command), round(Tilt_PID_Controller.Command))
+            
     else:
-      print(('Frame %3d:' % (Frame)))
+      print ('Frame %3d:' % (Frame))
 
       # Display all the blocks in the frame #
       for Index in range (0, Count):
-        Display_Block (Index, Blocks[Index])
+        Display_Block (Index, Blocks[Index], 2048)
 
       # Find an acceptable block to lock on to #
       if Blocks[0].m_age > MINIMUM_BLOCK_AGE_TO_LOCK:
